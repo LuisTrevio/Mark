36371f829c95d2ced9861d7cb9ff14b7f974259f
LuisTrevio <97487146+LuisTrevio@users.noreply.github.com>
2022-01-12 02:40:33 -0600

    Add files via upload

--- js/funcionmarking.js
+++ js/funcionmarking.js
@@ -3,6 +3,5 @@ const checkbox = document.getElementById('checkbox');
     checkbox.addEventListener('change', () => {
     
         document.querySelectorAll('.menuon').forEach((result) => {result.classList.toggle('menu-toggle');})
-       
     })
     
