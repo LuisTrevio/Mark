b464ae4ecbcb00af8886437f88e0903a983a5e81
LuisTrevio <97487146+LuisTrevio@users.noreply.github.com>
2022-01-16 23:08:16 -0600

    Delete Mark-main directory

--- Mark-main/js/funcionmarking.js
+++
@@ -1,7 +0,0 @@
-const checkbox = document.getElementById('checkbox');
-
-    checkbox.addEventListener('change', () => {
-    
-        document.querySelectorAll('.menuon').forEach((result) => {result.classList.toggle('menu-toggle');})
-    })
-    
